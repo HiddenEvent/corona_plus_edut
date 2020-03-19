9a84d4a28e4f8691b1ac234f3be37206f98b45a1
RichardKim <hiddeneventor@gmail.com>
2020-03-19 18:01:08 +0900

    API 파씽 해보잣

---
+++ lib/corona_vo.dart
@@ -0,0 +1,12 @@
+
+class CoronaVO {
+  String _title;
+  String _totNum;
+  String _beforeNum;
+
+  String get title => _title;
+  String get totNum => _totNum;
+  String get before => _beforeNum;
+
+
+}
